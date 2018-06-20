7c53500064cb8ecc853bb395b9fae773eebb86f3
Yi An Lin <lois.kiro@gmail.com>
2018-06-20 20:54:53 +0800

    upload KendoGird

---
+++ KendoGrid/KendoGrid/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace KendoGrid.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ KendoGrid/KendoGrid/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace KendoGrid.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
